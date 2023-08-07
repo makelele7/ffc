f6d67946bc86de87808537d35d7c740342c2d91b
makelele7 <62468116+makelele7@users.noreply.github.com>
2023-08-07 19:33:47 +0200

    Add files via upload

---
+++ js/listclr.js
@@ -0,0 +1,13 @@
+const list = document.getElementById("latest-matches");
+const rows = list.getElementsByTagName("ul");
+
+for (var i = 0; i < rows.length; i++) {
+  var cells = rows[i].getElementsByTagName("li");
+  var result = cells[6].textContent.trim();
+
+  if (result === "win") {
+    cells[5].classList.add("win-text");
+  } else if (result === "lose") {
+    cells[5].classList.add("lose-text");
+  }
+}
